36bcd41444b22fe90c41baa9d2a3b97630d3c3cd
MandelaByron <mandelabyron@gmail.com>
2024-06-18 09:23:43 +0300

    registration

---
+++ 07-Register-Users/api/viewsets/usersViewsets.py
@@ -0,0 +1,62 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from users.models import User
+from api.serializers.users import UserSerializer, RegisterSerializer
+
+
+class UserViewset(viewsets.ModelViewSet):
+    
+    http_method_names = ['get', 'patch', "delete"]
+    
+    serializer_class = UserSerializer
+    
+    queryset =  User.objects.all()
+    
+    
+    def get_object(self):
+        user = User.objects.get_object_by_public_id(self.kwargs['pk'])
+        
+        return user
+
+
+
+class RegisterViewset(viewsets.ViewSet):
+    #list(), create(), retrieve(), update(), partial_update(),destroy()
+
+    def create(self, request):
+        
+        serializer = RegisterSerializer(data=request.data)
+        
+        serializer.is_valid(raise_exception=True)
+        
+        serializer.save()
+        
+        return Response(
+            
+            {
+                "user": serializer.data
+            },
+            
+            status=status.HTTP_201_CREATED
+            
+            
+        )
+
+        
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
