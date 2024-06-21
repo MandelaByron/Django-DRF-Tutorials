793a8fa140d5b9452af302b8f3e9e8614cf3ceb6
MandelaByron <mandelabyron@gmail.com>
2024-06-21 18:14:01 +0300

    JWT AUTH

---
+++ 08-JWT-Auth/api/viewsets/usersViewsets.py
@@ -0,0 +1,92 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+from users.models import User
+from api.serializers.users import UserSerializer, RegisterSerializer, LoginSerializer
+
+
+class UserViewset(viewsets.ModelViewSet):
+    
+    http_method_names = ['get', 'patch', "delete"]
+    
+    serializer_class = UserSerializer
+    
+    queryset =  User.objects.all()
+    
+    
+    def get_object(self):
+        user = User.objects.get_object_by_public_id(self.kwargs['pk'])
+        
+        return user
+
+
+
+class RegisterViewset(viewsets.ViewSet):
+    #list(), create(), retrieve(), update(), partial_update(),destroy()
+
+    def create(self, request):
+        
+        serializer = RegisterSerializer(data=request.data)
+        
+        serializer.is_valid(raise_exception=True)
+        
+        serializer.save()
+        
+        return Response(
+            
+            {
+                "user": serializer.data
+            },
+            
+            status=status.HTTP_201_CREATED
+            
+            
+        )
+
+
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        
+        return Response({"message": "This is a private view"}, status=status.HTTP_200_OK)
+    
+class PublicView(APIView):
+        
+    def get(self, request):
+        
+        return Response({"message": "This is a public view"}, status=status.HTTP_200_OK)   
+
+
+class LoginView(viewsets.ViewSet):
+    serializer_class = LoginSerializer
+    http_method_names = ['post']
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data = request.data)
+        
+        try:
+            serializer.is_valid(raise_exception=True)
+        
+        except TokenError as e:
+            raise InvalidToken(e)
+        
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+            
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
